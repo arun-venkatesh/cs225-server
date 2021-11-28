8399f2a1b1e5d0a0c3c15d649c46ed2fab5d04c8
R-Kolhatkar <R_S_KOLHATKAR@ucsb.edu>
2021-11-27 16:03:50 -0800

    Moving updated Handler.java to API

--- src/main/java/api/Handler.java
+++ src/main/java/api/Handler.java
@@ -8,10 +8,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.*;
+import org.postgis.*;
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin("*")
 public class Handler {
+
+	static final String DB_URL = "jdbc:postgresql://localhost:25432/SixFtTracing"; // Change SixFtTracing to name of database
+    static final String USER = "docker";
+    static final String PASS = "docker";
 
 	@PostMapping("/upload")
 	public String dumpData(@RequestBody String data) throws Exception {
@@ -40,6 +47,28 @@ public class Handler {
 			 *  }
 			 * ]
 			 */
+
+			Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			//((org.postgresql.PGConnection)conn).addDataType("geometry", PGgeometry.class);
+			//((org.postgresql.PGConnection)conn).addDataType("geography", PGgeography.class);
+			((org.postgresql.PGConnection)conn).addDataType("box3d", PGbox3d.class);
+			Statement stmt = conn.createStatement();
+		
+			for(int i = 0; i < obj.length(); i++) {
+				JSONObject entry = obj.getJSONObject(i);
+				long contact_time = (Long) entry.get("timeStamp");
+				String user_id_one = entry.get("sourceDevice").toString();
+				String user_id_two = entry.get("destinationDevice").toString();
+				double latitude = (Double) entry.get("latitude");
+				double longitude = (Double) entry.get("longitude");
+
+
+				String QUERY = "INSERT INTO \"ContactTracingMaster\"( \n" +
+                    "\tcontact_time, user_id_one, user_id_two, location_of_contact) \n" +
+                    "\tVALUES(TO_TIMESTAMP(" + contact_time + "), " + user_id_one + ", " + user_id_two + ", " + "POINT(" + longitude + " " + latitude + ")";
+				stmt.executeUpdate(QUERY);
+			}
+			
 			returnObj.put("message", "success");
 			returnObj.put("data", obj.toString()); //Currently included for testing
 			returnObj.put("status_code", 200);
