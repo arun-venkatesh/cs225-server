4f4924820cba3f3f7f9fb6f652ac0781f3f82324
arun-venkatesh <2arun.venkatesh@gmail.com>
2021-11-28 15:08:42 -0800

    Added SSL support (For Android) and scheduler test code

--- src/main/java/cs225/Cs225Application.java
+++ src/main/java/cs225/Cs225Application.java
@@ -1,13 +1,22 @@
 package cs225;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.BatchResponse;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
@@ -17,22 +26,48 @@ import api.Handler;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = Handler.class)
+@EnableScheduling
 public class Cs225Application {
+	
+	private static Boolean isInitCalled = Boolean.FALSE;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FirebaseMessagingException, IOException {
 		SpringApplication.run(Cs225Application.class, args);
+		init();
 	}
 
-    @Scheduled(cron = "0 * * * * *") //For now, runs every minute (Testing)
-    public void notifyUsers() throws FirebaseMessagingException {
+	private static void init() throws FirebaseMessagingException, IOException {
+		
+		isInitCalled = Boolean.TRUE;
+		System.out.println("Init called");
+		FileInputStream serviceAccount =
+				  new FileInputStream("cs225-middleware-firebase-adminsdk-v3hq4-3aa89a208a.json");
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+		  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+		  .build();
+
+		FirebaseApp.initializeApp(options);
+		System.out.println("Init Completed");
+	}
+	
+    @Scheduled(fixedRate = 60000) //For now, runs every minute (Testing)
+    public void notifyUsers() throws FirebaseMessagingException, IOException {
     	
+    	if(!isInitCalled)
+    	{
+    		init();
+    	}
     	//Process, fetch registeration_tokens and store it in the list below (registrationTokens)
     	
-    	List<String> registrationTokens = Arrays.asList();
+    	System.out.println("Inside Job");
+    	List<String> registrationTokens = Arrays.asList(
+    			"faW9g2JnQRSXPG6CV6IntC:APA91bEeg216szQjCmN42TcXLopOujaraCQfNtl10IqRAa8g3f0HyLumK6_EiEQA1lYCw9pWb16Wdv9ps3AIKaVCQi3xG8cx7kTN9tLCukV19GAptX6eU3UplYL0Exsfm38e2h58ebD2"
+    	);
 
 		MulticastMessage message = MulticastMessage.builder()
 		    .putData("type", "notification")
-		    .putData("message", "You have been in contact with a COVID symptomatic/positive person. Please report this and take precautionary measures")
+		    .putData("message", "You have been in contact with a COVID symptomatic/positive person. Please report this and take precautionary measures.")
 		    .addAllTokens(registrationTokens)
 		    .build();
 		BatchResponse response = FirebaseMessaging.getInstance().sendMulticast(message);
